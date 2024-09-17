62ab1e0881f4f3b2bdddbd2f601abaf96d0366e4
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-17 16:27:48 +0530

    "Extracted header and post components from App.js, replacing hardcoded HTML with imports and function calls."

---
+++ client/src/post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <div className="image">
+        <img src="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/AVPN_APAC_SSF_2_HERO.width-600.format-webp.webp" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Indian non-profits are leveraging technology and AI to tackle threats like higher temperatures and water scarcity</h2>
+        <p className="info">
+          <a className="author">Amal Anillkumar</a>
+          <time>2024-09-17 15:56</time>
+        </p>
+        <p className="summary">With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
+</p>
+      </div>
+      </div>
+       
+    );
+}
