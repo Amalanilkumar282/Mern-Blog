aaae8b1d5895e2be044063048e74601ae6ef40c3
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-17 15:53:59 +0530

    "Updated App.css with new styles for img, header, and post elements. Added post components with images and text to App.js."

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,47 @@ function App() {
   return (
    <main>
     <header>
-    <a href="">My Blog</a>
+    <a href="" className="logo">My Blog</a>
     <nav>
       <a href="">Login</a>
       <a href="">Register</a>
     </nav>
     </header>
+      <div className="post">
+        <div className="image">
+        <img src="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/AVPN_APAC_SSF_2_HERO.width-600.format-webp.webp" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Indian non-profits are leveraging technology and AI to tackle threats like higher temperatures and water scarcity</h2>
+        <p>With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
+Building climate resilience is therefore an urgent need. Changemakers from across India and the Asia-Pacific region have been answering this call with innovative digital-led solutions, even leveraging AI to deepen and scale their impact.
+To support their development, our philanthropic arm Google.org supported the APAC Sustainability Seed Fund founded by AVPN with Asian Development Bank as the outreach partner. We’ve been inspired by the progress and impact the Indian recipients of this fund have achieved:</p>
+      </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+        <img src="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/AVPN_APAC_SSF_2_HERO.width-600.format-webp.webp" alt=""/>
+        </div>
+        <div className="texts">
+        <h2>Indian non-profits are leveraging technology and AI to tackle threats like higher temperatures and water scarcity</h2>
+        <p>With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
+Building climate resilience is therefore an urgent need. Changemakers from across India and the Asia-Pacific region have been answering this call with innovative digital-led solutions, even leveraging AI to deepen and scale their impact.
+To support their development, our philanthropic arm Google.org supported the APAC Sustainability Seed Fund founded by AVPN with Asian Development Bank as the outreach partner. We’ve been inspired by the progress and impact the Indian recipients of this fund have achieved:</p>
+      </div>
+      </div>
+
+      <div className="post">
+      <div className="image">
+        <img src="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/AVPN_APAC_SSF_2_HERO.width-600.format-webp.webp" alt=""/>
+        </div><div className="texts">
+        <h2>Indian non-profits are leveraging technology and AI to tackle threats like higher temperatures and water scarcity</h2>
+        <p>With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
+Building climate resilience is therefore an urgent need. Changemakers from across India and the Asia-Pacific region have been answering this call with innovative digital-led solutions, even leveraging AI to deepen and scale their impact.
+To support their development, our philanthropic arm Google.org supported the APAC Sustainability Seed Fund founded by AVPN with Asian Development Bank as the outreach partner. We’ve been inspired by the progress and impact the Indian recipients of this fund have achieved:</p>
+      </div>
+      </div>
+      
    </main>
   );
 }
