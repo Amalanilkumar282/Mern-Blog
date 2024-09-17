491dbcaadde67e96fb86225260a96c0abbcd99f1
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-17 19:44:35 +0530

    "Added express.json middleware and modified /register endpoint to extract username and password from request body"

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const express=require('express');
 const cors= require('cors');
+const { request } = require('http');
 const app=express();
 
 app.use(cors());
+app.use(express.json());
 
 app.post('/register',(req,res)=>{
-    res.json('test ok3');
+    const {username,password}=req.body;
+    res.json({requestData:{username,password}});
     
 });
 
