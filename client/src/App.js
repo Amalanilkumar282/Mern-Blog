cab4371c555f1569d1eb8cdcca96a5d124b11b04
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-17 16:05:27 +0530

    "Added CSS styles for post info and summary, and updated App.js to include author, time, and summary elements in post texts"

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,11 @@ function App() {
         </div>
         <div className="texts">
         <h2>Indian non-profits are leveraging technology and AI to tackle threats like higher temperatures and water scarcity</h2>
-        <p>With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
+        <p className="info">
+          <a className="author">Amal Anillkumar</a>
+          <time>2024-09-17 15:56</time>
+        </p>
+        <p className="summary">With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
 Building climate resilience is therefore an urgent need. Changemakers from across India and the Asia-Pacific region have been answering this call with innovative digital-led solutions, even leveraging AI to deepen and scale their impact.
 To support their development, our philanthropic arm Google.org supported the APAC Sustainability Seed Fund founded by AVPN with Asian Development Bank as the outreach partner. We’ve been inspired by the progress and impact the Indian recipients of this fund have achieved:</p>
       </div>
@@ -28,18 +32,27 @@ To support their development, our philanthropic arm Google.org supported the APA
         </div>
         <div className="texts">
         <h2>Indian non-profits are leveraging technology and AI to tackle threats like higher temperatures and water scarcity</h2>
-        <p>With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
+        <p className="info">
+          <a className="author">Amal Anillkumar</a>
+          <time>2024-09-17 15:56</time>
+        </p>
+        <p className="summary">With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
 Building climate resilience is therefore an urgent need. Changemakers from across India and the Asia-Pacific region have been answering this call with innovative digital-led solutions, even leveraging AI to deepen and scale their impact.
 To support their development, our philanthropic arm Google.org supported the APAC Sustainability Seed Fund founded by AVPN with Asian Development Bank as the outreach partner. We’ve been inspired by the progress and impact the Indian recipients of this fund have achieved:</p>
       </div>
       </div>
 
       <div className="post">
-      <div className="image">
+        <div className="image">
         <img src="https://storage.googleapis.com/gweb-uniblog-publish-prod/images/AVPN_APAC_SSF_2_HERO.width-600.format-webp.webp" alt=""/>
-        </div><div className="texts">
+        </div>
+        <div className="texts">
         <h2>Indian non-profits are leveraging technology and AI to tackle threats like higher temperatures and water scarcity</h2>
-        <p>With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
+        <p className="info">
+          <a className="author">Amal Anillkumar</a>
+          <time>2024-09-17 15:56</time>
+        </p>
+        <p className="summary">With global temperatures and emissions expected to continue rising, the adverse impact of climate change events like floods and heatwaves are also expected to intensify1. In India, a large number of communities are vulnerable to such climate-related disasters2.
 Building climate resilience is therefore an urgent need. Changemakers from across India and the Asia-Pacific region have been answering this call with innovative digital-led solutions, even leveraging AI to deepen and scale their impact.
 To support their development, our philanthropic arm Google.org supported the APAC Sustainability Seed Fund founded by AVPN with Asian Development Bank as the outreach partner. We’ve been inspired by the progress and impact the Indian recipients of this fund have achieved:</p>
       </div>
