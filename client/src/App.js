34b0c61b83fb86a7b4a113fadc7f2896eb9dec53
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-17 16:50:21 +0530

    "Added React Router to App.js and Header.js, and wrapped App in BrowserRouter in index.js"

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import Post from "./post";
 import Header from "./Header";
+import {Route,Routes} from "react-router-dom";
 
 function App() {
   return (
-   <main>
-      <Header/>
-      <Post/>
-      <Post/>
-      <Post/>
-   </main>
+    <Routes>
+      <Route index element={
+         <main>
+         <Header/>
+         <Post/>
+         <Post/>
+         <Post/>
+      </main>
+      }/>
+      <Route path={'/login'} element={
+        <main>
+          <Header/>
+        <div>login page</div>
+        </main>
+      }/>
+    </Routes>
+  
   );
 }
 
