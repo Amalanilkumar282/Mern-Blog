adb1f29fc4394e98a853e93c89807643634c24d7
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-18 15:07:10 +0530

    "Removed MongoDB connection and User model, commented out User.create in register endpoint, and updated RegisterPage.js with minor formatting changes."

--- api/index.js
+++ api/index.js
@@ -1,20 +1,26 @@
 const express=require('express');
 const cors= require('cors');
-const mongoose = require("mongoose");
-const User= require('./model/User');
+// const mongoose = require("mongoose");
+//const User= require('./model/User');
 const app=express();
 
-app.use(cors());
-app.use(express.json());
+ app.use(cors());
+ app.use(express.json());
 
-mongoose.connect('mongodb+srv://amalanilkumaredu:Amal1234@cluster0.iyjwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+ //mongoose.connect('mongodb+srv://amalanilkumaredu:Amal1234@cluster0.iyjwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
-app.post('/register',async (req,res) => {
-    const {username,password}=req.body;
-    console.log(username);
-   const userDoc=await User.create({username,password});
-    res.json({requestData:{username,password}});
+ app.post('/register',async (req,res) => {
+     const {username,password}=req.body;
+     console.log(username);
+     console.log(password);
+    //const userDoc=await User.create({username,password});
+     res.json({requestData:{username,password}});
     
-});
+ });
+
+
+// app.post('/register',(req,res)=>{
+//     res.json('test ok');
+// });
 
 app.listen(4000);
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -16,7 +16,7 @@ export default function RegisterPage(){
             <h1>Register</h1>
         <input type="text" placeholder="username" 
             value={username}
-            onChange={ev=> setUsername(ev.target.value)}/>
+            onChange={ev => setUsername(ev.target.value)}/>
         <input type="password" placeholder="password"
         value={password}
         onChange={ev=> setPassword(ev.target.value)}/>
