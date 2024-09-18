9ac47d854614cc65f6284ad4ce288ee65fb83484
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-19 00:15:54 +0530

    "Removed import of 'response' from 'express', uncommented import, and reformatted code with consistent indentation."

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import { response } from "express";
+//import { response } from "express";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 
@@ -9,23 +9,24 @@ export default function LoginPage(){
     const [redirect,setRedirect]=useState(false);
     async function login(ev){
         ev.preventDefault();
-        const response=await fetch('http://localhost:4000/login',{
+        const response = await fetch('http://localhost:4000/login',{
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'Content-Type': 'application/json'},
             credentials: 'include',
         });
-        if(response.ok){
-            setRedirect(true);
-        }else {
-            alert('wrong credentials');
+         if(response.ok){
+             setRedirect(true);
+         }else {
+             alert('wrong credentials');
+         }
         }
 
-    }
+     
 
-    if (redirect){
-        return <Navigate to={'/'} />
-    }
+     if (redirect){
+         return <Navigate to={'/'} />
+     }
     return(
     <form className="login" onSubmit={login}>
         <h1>Login</h1>
