47465894eb9334c89d8432f2fb110fb7316b749f
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-18 15:32:02 +0530

    solved previous error,finding connection issue

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
 const express=require('express');
 const cors= require('cors');
-// const mongoose = require("mongoose");
-//const User= require('./model/User');
+const mongoose = require("mongoose");
+const User= require('./models/User');
 const app=express();
 
  app.use(cors());
  app.use(express.json());
 
- //mongoose.connect('mongodb+srv://amalanilkumaredu:Amal1234@cluster0.iyjwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+  mongoose.connect('mongodb+srv://amalanilkumaredu:Amal1234@cluster0.iyjwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
  app.post('/register',async (req,res) => {
      const {username,password}=req.body;
      console.log(username);
      console.log(password);
-    //const userDoc=await User.create({username,password});
+    const userDoc=await User.create({username,password});
      res.json({requestData:{username,password}});
     
  });
--- api/models/User.js
+++ api/models/User.js
@@ -1,11 +1,11 @@
 const mongoose= require('mongoose');
-const {Schema, model} = mongoose;
+//const {Schema, model} = mongoose;
 
-const UserSchema= new  Schema({
+const UserSchema= new  mongoose.Schema({
     username: {type: String, required: true, min:4, unique:true},
     password: {type: String, required: true},
 });
 
-const UserModel= model('User',UserSchema);
+const UserModel= mongoose.model('User',UserSchema);
 
-module.exports=UserModel;
+module.exports= UserModel;
