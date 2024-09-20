05c73eb1a60a8c66d6b1ae2f934c867c1f5f1272
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-20 15:08:56 +0530

    "Added state variables for title, summary, and content, and configured ReactQuill with modules and formats. Replaced plain ReactQuill component with configured one and added a create post button."

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
 export default function CreatePost(){
+    const [title,setTitle]=useState('');
+    const [summary,setSummary]=useState('');
+    const [Content,setContent]=useState('');
+    const modules = {
+        toolbar: [
+          [{ 'header': [1, 2, false] }],
+          ['bold', 'italic', 'underline','strike', 'blockquote'],
+          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+          ['link', 'image'],
+          ['clean']
+        ],
+      };
+    const formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+      ];
     return(
         <form>
             <input type='title' placeholder={"title"}/>
             <input type='summary' placeholder={"summary"}/>
             <input type='file'/>
-            <ReactQuill/>
+            <ReactQuill value={Content} modules={modules} formats={formats}/>
+            <button style={{marginTop:'5px'}}>Create post</button>
         </form>
     )
 }
