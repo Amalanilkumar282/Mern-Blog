5e972fc36e0e5e93769aa1983bdf66f2ef16692e
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-20 15:17:27 +0530

    "Added state variables and event handlers for title and summary inputs, and updated ReactQuill component with onChange event handler."

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,31 +2,38 @@ import { useState } from "react";
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 
+const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      ['clean']
+    ],
+  };
+const formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ];
+
 export default function CreatePost(){
     const [title,setTitle]=useState('');
     const [summary,setSummary]=useState('');
     const [Content,setContent]=useState('');
-    const modules = {
-        toolbar: [
-          [{ 'header': [1, 2, false] }],
-          ['bold', 'italic', 'underline','strike', 'blockquote'],
-          [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-          ['link', 'image'],
-          ['clean']
-        ],
-      };
-    const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-      ];
+   
     return(
         <form>
-            <input type='title' placeholder={"title"}/>
-            <input type='summary' placeholder={"summary"}/>
+            <input type='title' placeholder={"title"} 
+            value={title} onChange={ev=>setTitle(ev.target.value)}/>
+            <input type='summary' placeholder={"summary"}
+            value={summary} onChange={ev=>setSummary(ev.target.value)}/>
             <input type='file'/>
-            <ReactQuill value={Content} modules={modules} formats={formats}/>
+            <ReactQuill 
+            value={Content} 
+            onChange={newValue=>setContent(newValue)}
+            modules={modules} formats={formats}/>
             <button style={{marginTop:'5px'}}>Create post</button>
         </form>
     )
