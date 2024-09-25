66d58da2d479f5454972bf7e7a5ffd4b7f0fc23a
Amal Anilkumar <amalanilkumar282@gmail.com>
2024-09-26 02:06:05 +0530

    "Added useEffect hook to IndexPage to fetch data from '/post' endpoint"

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -1,5 +1,14 @@
+import { useEffect } from "react";
 import Post from "../post"
 export default function IndexPage() {
+    useEffect(
+        () => {
+            fetch('/post').then(Response=>{
+                Response.json(posts=>{
+                    console.log(posts);
+            });
+        });
+        },[]);
     return (
         <div>
         <Post/>
